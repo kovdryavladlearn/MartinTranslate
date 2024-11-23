1d79447c2e09e07cb80798e42e56291379ecd17c
Vlad Kovdria <kovdryavlad@gmail.com>
2024-11-23 14:36:14 +0200

    chore: use seeds for any environment and use exception handler on the top

--- MartinTranslate/src/Web/Program.cs
+++ MartinTranslate/src/Web/Program.cs
@@ -9,16 +9,9 @@ builder.Services.AddWebServices();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    await app.InitialiseDatabaseAsync();
-}
-else
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+app.UseExceptionHandler(options => { });
+
+await app.InitialiseDatabaseAsync();
 
 app.UseHealthChecks("/health");
 app.UseStaticFiles();
@@ -30,8 +23,6 @@ app.UseSwaggerUi(settings =>
 });
 
 app.MapFallbackToFile("index.html");
-
-app.UseExceptionHandler(options => { });
 
 app.Map("/", () => Results.Redirect("/swagger"));
 
